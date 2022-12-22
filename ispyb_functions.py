3bdfe693b1a673f6333ae8ee98305dda28454748
Jun Aishima <jaishima@bnl.gov>
2022-12-21 19:29:01 -0500

    try to get NSLS2 usernames from ISPyB proposals

--- ispyb_functions.py
+++ ispyb_functions.py
@@ -1,6 +1,8 @@
 import ispyb_lib
 import nsls2api_lib
 
-proposal_ids = ispyb_lib.get_ispyb_proposals()
-for proposal_id in proposal_ids:
-    print(f"usernames from proposal {proposal_id} are: {nsls2api_lib.get_usernames_from_proposal(proposal_id)}")
+proposal_numbers = ispyb_lib.get_proposal_numbers(ispyb_lib.get_ispyb_proposal_ids())
+print(proposal_numbers)
+for proposal_number in proposal_numbers:
+    print(f"usernames from proposal {proposal_number} are: {nsls2api_lib.get_usernames_from_proposal(proposal_number)}")
+print(f"proposal numbers: {ispyb_lib.get_proposal_numbers(ispyb_lib.get_ispyb_proposal_ids())}")
