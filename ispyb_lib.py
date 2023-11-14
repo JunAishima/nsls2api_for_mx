c52811b6abe0eb8fab9071fb404446249b48a259
Jun Aishima <jaishima@bnl.gov>
2023-11-14 15:15:02 -0500

    fix delete query to remove from Session_has_Person

--- ispyb_lib.py
+++ ispyb_lib.py
@@ -108,8 +108,11 @@ def remove_all_usernames_for_proposal(proposal_id, dry_run=True):
     if not dry_run:
         for session_id in session_ids:
             for person in people_in_sessions:
-                query = f"DELETE Session_has_Person where sessionId={session_id},personId={person.person_id}"
-                delete_session_has_person = queryDB(query)
+                query = f"DELETE FROM Session_has_Person where sessionId={session_id[0]} AND personId={person}"
+                try:
+                    delete_session_has_person = queryDB(query)
+                except mysql.connector.errors.ProgrammingError as e:
+                    print(f"Problem with proposal {proposal_id}, session {session_id}, person {person}")
 
 
 def create_person(first_name, last_name, login, is_pi, dry_run=True):
